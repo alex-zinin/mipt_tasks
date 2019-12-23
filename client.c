fa1cdf6284291b370a846db1b7a3aa6ba45abe00
alex-zinin <43748062+alex-zinin@users.noreply.github.com>
2019-12-23 09:26:31 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,24 +1,22 @@
 #include<stdio.h>
-#include<sys.socket.h>
+#include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<string.h>
-#define PORT 8080
+#define PORT 5051
 
-int main()
+int main(int argc, char const *argv[])
 {
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
   char buffer[1024] = {0};
+  /* Создаем TCP сокет */
   if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("\n Socket creation error \n");
     return -1;
 
-
-
-
-  }
+}
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
@@ -26,28 +24,23 @@ int main()
 
 
 
-
-
-
-  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)  // строка -> в структуру сетевого адреса
   {
     printf("Not correct address");
     return -1;
 
   }
 
-  if(connect(sock,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
   {
-    printf("Connection failed");
+    printf("Connection failed\n");
     return -1;
   }
 
 
 
 
-
-
-  int pid = fork();
+int pid = fork();
   if(pid != 0 )
   {
     while(1)
@@ -68,7 +61,7 @@ int main()
     {
       fgets(buffer, 1023, stdin);
       send(sock, buffer, strlen(buffer), 0);
-      memeset(buffer, 0, 1024);
+      memset(buffer, 0, 1024);
     }
   }
 
